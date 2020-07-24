f7c2e9f6b6f7b82e7dc07f67551d119c5c77d78c
Nikolashin D <dg.nikolashin@jet.su>
2020-07-24 20:07:42 +0300

    Add infrastructure

---
+++ core/src/test/kotlin/org/sefran/heimdallr/core/CoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.sefran.heimdallr.core
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
